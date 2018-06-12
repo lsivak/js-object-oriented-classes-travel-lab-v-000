a761bd682eef0e4c56bd34de35adcc9c4339ddc6
Lara Sanders <larasanders@gmail.com>
2018-06-12 18:36:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+yearsExperienceFromBeginningOf(year) {
+  let endDate = new Date(year, 1)
+  let years = Math.ceil((endDate - this.startDate)/31556952000)
+  return years
+}
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+      this.beginningLocation = beginningLocation
+      this.endingLocation = endingLocation
+  }
+
+aveToIndex(avenue) {
+return eastWest.indexOf(avenue)
+}
+
+blocksTravelled() {
+  let horizontalNum = this.aveToIndex(this.endingLocation.horizontal) - this.aveToIndex(this.beginningLocation.horizontal)
+  let verticalNum =  (this.endingLocation.vertical) - (this.beginningLocation.vertical)
+  return horizontalNum + verticalNum
+    }
+
+estimatedTime(peakTravel) {
+  if(peakTravel)
+  return this.blocksTravelled()/ 2
+  else
+  return this.blocksTravelled()/3
+
+}
+}
