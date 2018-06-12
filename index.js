6a497d90f72e7a43813dbedaf7814868ac4ab874
Lara Sanders <larasanders@gmail.com>
2018-06-12 17:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Route {
       this.endingLocation = endingLocation
   }
 
-aveToIndex(beginningLocation, endingLocation) {
+aveToIndex() {
        debugger
     let numAve = (eastWest.indexof(this.endingLocation.horizontal) - eastWest.indexof(this.beginningLocation.horizontal))
     debugger
