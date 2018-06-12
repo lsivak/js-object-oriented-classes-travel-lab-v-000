579a0011e0b9c6e81b30c755ca732400beaa7f0b
Lara Sanders <larasanders@gmail.com>
2018-06-12 16:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = new Date(startDate)
   }
 yearsExperienceFromBeginningOf(year) {
-  let endDate = new Date(year, 1)
+  let endDate = new Date(year, 1, 1)
   let years = Math.floor((endDate - this.startDate)/31556952000)
   return years
 }
